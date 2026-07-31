b7549ab145a9c0378043252a515aaa918ee258cd
adityaexe-23 <dugaraditya2305@gmail.com>
2026-07-31 12:38:26 +0530

    made changes in program11

--- program11.c
+++ program11.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 int main(){
     char day;
-    printf("Enter a character for days like M, T, W, Th, F, S, Su");
-    scanf("%s", &day);
+    printf("Enter a character for days like M, T, W, t, F, S, s: ");
+    scanf(" %c", &day);
+    
     switch(day)
     {
         case 'M': printf("The day is Monday");
             break;
-        case 'T': printf("The day is Monday");
+        case 'T': printf("The day is Tuesday");
             break;
-        case 'W': printf("The day is Monday");
+        case 'W': printf("The day is Wednesday");
             break;
-        case 'Th': printf("The day is Monday");
+        case 't': printf("The day is Thursday");
             break;
-        case 'F': printf("The day is Monday");
+        case 'F': printf("The day is Friday");
             break;
-        case 'S': printf("The day is Monday");
+        case 'S': printf("The day is Saturday");
             break;
-        case 'Su': printf("The day is Monday");
+        case 's': printf("The day is Sunday");
             break;
         default: printf("Invalid Input");
             break;
     }
+    return 0;
 }
